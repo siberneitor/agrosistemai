eaef509607d8480114ffa5c8c609d70f4b5def40
siberneitor <siberneitor@gmail.com>
2020-07-23 01:20:35 -0500

    se quita redireccionamieto original a página index

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <html>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<meta http-equiv="refresh" content="0;URL=views/puntoVenta.php">
+<!--<meta http-equiv="refresh" content="0;URL=views/puntoVenta.php"> -->
+<a href="/views/puntoVenta.php">principio</a>
 <!--
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
