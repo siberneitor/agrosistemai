188b6d75af8e42739d644697ce7fed5d6c977ab7
siberneitor <siberneitor@gmail.com>
2020-08-17 21:45:59 -0500

    se eleiminar todos los archivos

--- controllers/AJAX/eliminarCobrar.php
+++
@@ -1,5 +0,0 @@
-<?php
-include '../../database/conexioni.php';
-	$resetearTemporal=$mysqli->query("delete from temporal2");
-	$resetearInvTemp=$mysqli->query("delete from inventarioTempXprod");
-?>
--- controllers/variables.php
+++
@@ -1,4 +0,0 @@
-<?php
-$fechaActual = date("Y-m-d");
-$fechaHoraActual = date("Y-m-d H:i:s");
-?>
